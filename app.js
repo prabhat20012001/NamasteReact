83898fc301a74f931a25dede063d941e2f6391a4
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2023-11-04 01:18:58 +0530

    compare jsx and core react component

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-/**
- * <div id="parent">
- *   <div id="child">
- *          <h1> i am h1 tag</h1>
- *          <h1> i am another h2 tag</h1>
- *    </div>
- *  <div id="child2">
- *          <h1> i am h1 tag</h1>
- *          <h1> i am another h2 tag</h1>
- *    </div>
- * </div>
- * 
- * 
- * 
- */
+const heading=React.createElement("h1",{id:"heading"},"Namaste React")
 
-
-const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"}),[React.createElement("h1",{},"i am an h1 tag"),React.createElement("h1",{},"i am another h1 tag")],React.createElement("div",{id:"child"}),[React.createElement("h1",{},"i am an h1 tag"),React.createElement("h1",{},"i am another h1 tag")]])
-
-
-console.log(parent)  //will return object
+console.log(heading)
+const jsxHeading=<h1 id="heading">Namaste React jsx heading</h1>
+console.log(jsxHeading)
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
 
 
 
 
-
-
-
-// const heading=React.createElement("h1",{id:"heading",class:"heading"},"hello world from react!")
-
-// console.log(heading)  //will return object
-// const root=ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading) //render method is responsible to take the object and put it up and convert into heading tag and put it up in the dom
+root.render(heading)
